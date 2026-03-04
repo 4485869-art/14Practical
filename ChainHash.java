73a9e72938e5ee8b46ed8a9eea3b43531c27ae36
4485869-art <4485869@myuwc.ac.za>
2026-03-04 13:42:54 +0000

    Constructing a table for ChainHash method

--- ChainHash.java
+++ ChainHash.java
@@ -13,3 +13,21 @@ public class ChainedHash {
             this.value = value;
         }
     }
+        }
+
+    private LinkedList<Node>[] table;  // Array of linked lists
+    private int m;                       // Table size
+    private int size;                     // Number of items inserted
+
+    // Construct
+    @SuppressWarnings("unchecked")
+    public ChainedHash(int tableSize) {
+        this.m = tableSize;
+        this.table = new LinkedList[m + 1];  // indices 1 to m
+        this.size = 0;
+
+        // Initialize each slot with an empty linked list
+        for (int i = 1; i <= m; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
