15d190daa36164193cec67be3d28bf92ecffb0ed
4485869-art <4485869@myuwc.ac.za>
2026-03-04 13:18:05 +0000

    Created main method

---
+++ OpenHash.java
@@ -0,0 +1,20 @@
+
+// OpenHash.java
+public class OpenHash {
+
+    // Node class to store key-value pairs
+    private static class Node {
+        int key;
+        String value;
+
+        Node(int key, String value) {
+            this.key = key;
+            this.value;
+            
+
+        Node(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
