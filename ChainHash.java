f10a78a0f34f7aab7ea8c0cf9ec2aa99ba0d5f37
4485869-art <4485869@myuwc.ac.za>
2026-03-04 13:39:19 +0000

    Import java package

---
+++ ChainHash.java
@@ -0,0 +1,9 @@
+// ChainedHash.java
+import java.util.LinkedList;
+
+public class ChainedHash {
+
+    // Node class to store key-value pairs
+    private static class Node {
+        int key;
+        String value;
