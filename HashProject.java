9c8540f1b0b951ae13eb8a727f1e9d264b906d92
4485869-art <4485869@myuwc.ac.za>
2026-03-04 12:58:17 +0000

    Generated one milion keys N

--- HashProject.java
+++ HashProject.java
@@ -1,6 +1,25 @@
 //4485869
 //Themba Sithole
 
-//Import then java packages
-import java.text.*;
-import java.util.*;
+// DataGenerator.java
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DataGenerator {
+
+    // N = 2^20 = 1,048,576
+    public static final int N = 1 << 20;
+
+    // Pair class to hold key-value pairs
+    public static class Pair {
+        public int key;
+        public String value;
+
+        public Pair(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    }
