82e6c671fa8680792008a9e7c7b9d1925726d413
4485869-art <4485869@myuwc.ac.za>
2026-03-04 13:55:02 +0000

    Starting Hash Experiment

--- ChainHash.java
+++ ChainHash.java
@@ -53,5 +53,46 @@ public class ChainedHash {
             }
         }
 
+        // Key not found - add new node to the END of the list
+        chain.add(new Node(key, value));
+        size++;
+    }
+
+    // Look up a key and return its value
+    public String lookup(int key) {
+        int index = hash(key);
+        LinkedList<Node> chain = table[index];
+
+        // Search the chain
+        for (Node node : chain) {
+            if (node.key == key) {
+                return node.value;  // Found it
+            }
+        }
+        return null;  // Not found
+    }
+
+    // Check if key is in table
+    public boolean isInTable(int key) {
+        return lookup(key) != null;
+    }
+
+    // Get current load factor
+    public double loadFactor() {
+        return (double) size / m;
+    }
+
+    // Clear the table for new experiment
+    @SuppressWarnings("unchecked")
+    public void clear() {
+        table = new LinkedList[m + 1];
+        for (int i = 1; i <= m; i++) {
+            table[i] = new LinkedList<>();
+        }
+        size = 0;
+    }
+}
+
+
 
 
